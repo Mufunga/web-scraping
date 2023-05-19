be4954cd240bd3574fbe37d3a01a4a2f8f558c56
guelord Mufunga <gmufunga76@hotmail.com>
2023-05-19 21:50:50 +0200

    version 5.19.9h50

--- functions.py
+++ functions.py
@@ -67,31 +67,25 @@ def scrape_book(book_urls):
     book_data["review_rating"]= soup.find ("p",class_="star-rating").attrs["class"] [1]
     book_data ["image_url"] = soup.find('img')["src"]
     return book_data
-     
 
 
+def save_book_data_to_csv(book_data:list[dict]):
+    Path(CSV_DIR).mkdir(parents=True, exist_ok=True)
+    header = book_data[0].keys
+    with open("CSV_DIR" "books_data.csv", mode="w", encoding= "utf-8-sig", newline="") as file_csv:
+        writer= csv.DictWriter(file_csv, fieldnames= header,dialect= "excel")
+        writer.writeheader()
+        writer.writerows(book_data)
     
-    
-    
-        #names_of_categories.append(categories_names)
-
-    
-
-
-
 
 
 def main():
 
     """Main function"""
-    """create my CSV directory"""
-    Path(CSV_DIR).mkdir(parents=True, exist_ok=True)
-
 
     # Récupération de toutes les catés
     categories_urls = get_categories_urls()
     categories_urls.remove('https://books.toscrape.com/catalogue/category/books_1/index.html')
-
 
     # Pour chaque catégorie
     for category_url in categories_urls:
@@ -102,54 +96,8 @@ def main():
         for url_book in books_urls_by_category:
     #        Récupérer les data de chaque livre
             all_books_data = scrape_book(url_book)
-            print (all_books_data)
+        return all_books_data
 
     #    Sauvegarder en csv
-    #rows = [scrape_book(url)]
-    with open('C:/Users/MufungaK/Documents/OC/LIVRAIBLE/projet_2/csv_dir/cat_data_book4.csv', mode='w', encoding="utf-8") as csv_file:
-            headers = ["url", "title", "upc", "price_including_tax", "price_excluding_tax", "number_available","product_description","category",  "review_rating",  "image_url"]
-
-            writer = csv.DictWriter(csv_file, fieldnames=headers,delimiter=',')
-
-            writer.writeheader()
-            writer.writerows(all_books_data)
-    #data_book_csv()
-
-    #    Récupérer les images
-
-#link_categories_books= get_categories_urls()
-#print(link_categories_books)
-
-#url = "https://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html"
-
-#if "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"  in link_categories_books:
-  #cat_url ="https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
-  #cat_books_urls=get_books_urls_from_category(cat_url)
-  #cat_books_data=[]
-#for url in cat_books_urls:
-   #cat_books_data.append(scrape_book(url))
-    #print(cat_books_data)
-
-
-
-#if __name__ == "__main__":
-
-    #main()
+   
     
-#data_book_csv()
-
-
-
-#print(link_categories_books)
-   #print(all_books_url)
-    
-
-
-#page_url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
-#category_urls = scrape_category_books(page_url)
-#print(category_urls)
-#for link_url in category_urls:
-   # scrape_book(link_url)
-    #break
-   #print(link_url)
-

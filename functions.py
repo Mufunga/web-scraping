e33de1aff1d8b48c3d2a6a0bdf624fff876c2fe2
guelord Mufunga <gmufunga76@hotmail.com>
2023-06-08 16:54:32 +0200

    Version 6.8.2023

--- functions.py
+++ functions.py
@@ -8,12 +8,18 @@ BOOK_SITE ="https://books.toscrape.com/"
 CSV_DIR= "data/csv"
 IMG_DIR = "data/img"
 
+def get_soup(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    if not response.ok:
+        print("erreur")
+        exit()
+    content = response.content
+    return BeautifulSoup(content, "html.parser")
+
 
 def get_categories_urls():
     category_uls = []
-    response = requests.get(BOOK_SITE)
-    page_cat = response.content
-    soup = BeautifulSoup(page_cat, "html.parser")
+    soup = get_soup(BOOK_SITE)
     ul_tag = soup.find("ul", class_="nav-list")
     li_tag = ul_tag.find_all("li")
     for ligne in li_tag:
@@ -24,20 +30,19 @@ def get_categories_urls():
 
 
 def get_books_urls_from_category(category_url):
+
     """Return the books urls_from_category"""
+
     book_urls = []
     a_tag_temp = "index.html"
     while True:
-        response = requests.get(category_url)
-        book_page = response.content
-        soup = BeautifulSoup(book_page, "html.parser")
+        soup = get_soup(category_url)
         link_book_categories = soup.find("ol")
         articles = link_book_categories.find_all("article", class_="product_pod")
         for article in articles:
             lien = article.find("a")["href"]
             chemin = lien.split("../")
             book_url = "https://books.toscrape.com/catalogue/" + chemin[3]
-            #print (book_urls)
             book_urls.append(book_url)
         next_b = soup.find("li", class_="next")
         if next_b:
@@ -50,9 +55,8 @@ def get_books_urls_from_category(category_url):
     return book_urls
 
 def scrape_book(book_urls):
-    reponse = requests.get(book_urls)
-    page = reponse.content
-    soup = BeautifulSoup(page, "html.parser")
+    
+    soup = get_soup(book_urls)
     td_list = soup.find_all("td")
     book_data = {}
     book_data ["title"]=soup.h1.text
@@ -68,33 +72,17 @@ def scrape_book(book_urls):
     a_list= soup.find_all("a")
     book_data["category"]= a_list[3].text
     book_data["review_rating"]= soup.find ("p",class_="star-rating").attrs["class"] [1]
-    if  book_data["review_rating"] == "One":
-         book_data["review_rating"]=1
-    elif  book_data["review_rating"] == "Two":
-         book_data["review_rating"]=2
-    elif  book_data["review_rating"] =="Three":
-         book_data["review_rating"]= 3
-    elif  book_data["review_rating"] == "Four":
-         book_data["review_rating"] = 4
-    elif book_data["review_rating"] == "Five":
-        book_data["review_rating"] = 5
-    else :
-         print()
-
+    book_data["review_rating"] = transform_review_rating_to_number( book_data["review_rating"])
     book_data ["image_url"] = soup.find('img')["src"]
     url_split= book_data["image_url"].split("../")
     book_data ["image_url"] = "https://books.toscrape.com/"+ url_split[2]
     return book_data
 
-#def transform_review_rating_to_number(review_rating :str) ->int :
-    #rating ={
-        #"one":1,
-        #"two": 2,
-        #"three": 3,
-        #"four": 4,
-        #"five" : 5
-   # }
-    #return rating.get(review_rating)
+def transform_review_rating_to_number(review_rating: str) -> int:
+
+    """Transform a rating string to rating number"""
+    rating = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    return rating.get(review_rating, "Valeur Inexistante")
 
 
 
@@ -142,10 +130,11 @@ def main():
     #        Récupérer les data de chaque livre
             book_data = scrape_book(url_book)
             books_data.append(book_data)
-            print(book_data["category"] + '  ' "data book  encours de telechargement veillez patientez...")
+            print(book_data["category"] + '  ' "data book  encours de telechargement veuillez patientez...")
             
     #    Sauvegarder en csv
         save_book_data_to_csv(books_data)
+        print(book_data["category"] + '  ' "data book  enregister sous format CSV veuillez patienter...")
 
         #Boucle de recuperation de l'image et sauvegarde
 
